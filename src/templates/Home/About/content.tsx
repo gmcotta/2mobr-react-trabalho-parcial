5e1a138e0554d648291a6f89eef83c4551206ac3
Gustavo Matias <gmcotta34@gmail.com>
2024-03-02 19:00:53 -0300

    refactor(About): add file for content

---
+++ src/templates/Home/About/content.tsx
@@ -0,0 +1,21 @@
+export const aboutContent = {
+  id: 'sobre',
+  title: 'Sobre mim',
+  texts: [
+    <>Eu sou <strong>Gustavo Matias</strong></>,
+    <>Desenvolvedor <strong>front-end</strong></>
+  ],
+  address: 'Guarulhos, SP',
+  description: 'Atuo na área de tecnologia da informação desde 2019, e como desenvolvedor front-end desde 2020, com foco em ReactJS.',
+  socialMediaTitle: 'Minhas redes: ',
+  socialMediaLinks: [
+    {
+      text: "LinkedIn",
+      href: "https://www.linkedin.com/in/gustavomatias/",
+    },
+    {
+      text: "GitHub",
+      href: "https://github.com/gmcotta",
+    },
+  ]
+};
--- src/templates/Home/About/index.tsx
+++ src/templates/Home/About/index.tsx
@@ -1,26 +1,20 @@
 import { Link } from "../../../components";
-import * as C from "./const";
+import * as C from "./content";
 import * as S from "./styles";
 
 function About() {
   return (
-    <S.About id="sobre">
+    <S.About id={C.aboutContent.id}>
       <S.Wrapper>
-        <S.AboutMe>Sobre mim</S.AboutMe>
-        <S.Text>
-          Eu sou <strong>Gustavo Matias</strong>
-        </S.Text>
-        <S.Text>
-          Desenvolvedor <strong>front-end</strong>
-        </S.Text>
-        <S.AddressText>Guarulhos, SP</S.AddressText>
-        <S.Description>
-          Atuo na área de tecnologia da informação desde 2019, e como
-          desenvolvedor front-end desde 2020, com foco em ReactJS.
-        </S.Description>
-        <S.SocialMediaTitle>Minhas redes:</S.SocialMediaTitle>
+        <S.AboutMe>{C.aboutContent.title}</S.AboutMe>
+        {C.aboutContent.texts.map((text, index) => (
+          <S.Text key={index}>{text}</S.Text>
+        ))}
+        <S.AddressText>{C.aboutContent.address}</S.AddressText>
+        <S.Description>{C.aboutContent.description}</S.Description>
+        <S.SocialMediaTitle>{C.aboutContent.socialMediaTitle}</S.SocialMediaTitle>
         <S.SocialMediaButtonContainer>
-          {C.links.map((link, index) => (
+          {C.aboutContent.socialMediaLinks.map((link, index) => (
             <Link key={index} href={link.href} target="_blank">
               {link.text}
             </Link>
