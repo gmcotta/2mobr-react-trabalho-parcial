7b5cac0803f13ef39b4ce2dbd7845184d3236db2
Gustavo Matias <gmcotta34@gmail.com>
2024-03-02 18:45:58 -0300

    refactor(Link): change component name

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,7 +3,7 @@ import * as S from "./styles";
 import { LinkProps } from "./link";
 
 const Link = memo(({ children, ...props }: LinkProps) => {
-  return <S.Button {...props}>{children}</S.Button>;
+  return <S.Link {...props}>{children}</S.Link>;
 });
 
 export { Link };
--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Button = styled.a`
+export const Link = styled.a`
     border: none;
     display: flex;
     align-items: center;
