2cd472685481dd6e77fcf1e5c7537515c567e81b
Gustavo Matias <gmcotta34@gmail.com>
2024-03-02 18:53:16 -0300

    refactor(Header): add file for texts

---
+++ src/templates/Home/Header/const.tsx
@@ -0,0 +1,16 @@
+export const headerTexts = {
+    title: <>Olá, eu sou <span>Gustavo</span></>,
+    subtitle: 'Desenvolvedor front-end',
+    emailLink: {
+        href: 'mailto:gmcotta34@gmail.com',
+        text: 'Entre em contato'
+    },
+    aboutLink: {
+        href: '#sobre',
+        text: 'Sobre mim'
+    },
+    image: {
+        src: 'https://avatars.githubusercontent.com/u/46160812?v=4',
+        alt: ''
+    }
+}
--- src/templates/Home/Header/index.tsx
+++ src/templates/Home/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Wrapper } from "../../../components";
+import * as C from "./const";
 import * as S from "./styles";
 
 function Header() {
@@ -6,21 +7,21 @@ function Header() {
     <S.Header>
       <Wrapper>
         <S.LeftContainer>
-          <S.Title>
-            Olá, eu sou <span>Gustavo</span>
-          </S.Title>
-          <S.Subtitle>Desenvolvedor front-end</S.Subtitle>
+          <S.Title>{C.headerTexts.title}</S.Title>
+          <S.Subtitle>{C.headerTexts.subtitle}</S.Subtitle>
           <S.LinkContainer>
-            <S.EmailLink href="mailto:gmcotta34@gmail.com">
-              Entre em contato
+            <S.EmailLink href={C.headerTexts.emailLink.href}>
+              {C.headerTexts.emailLink.text}
             </S.EmailLink>
-            <S.AboutLink href="#sobre">Sobre mim</S.AboutLink>
+            <S.AboutLink href={C.headerTexts.aboutLink.href}>
+              {C.headerTexts.aboutLink.text}
+            </S.AboutLink>
           </S.LinkContainer>
         </S.LeftContainer>
         <S.RightContainer>
           <S.Image
-            src="https://avatars.githubusercontent.com/u/46160812?v=4"
-            alt=""
+            src={C.headerTexts.image.src}
+            alt={C.headerTexts.image.alt}
           />
         </S.RightContainer>
       </Wrapper>
